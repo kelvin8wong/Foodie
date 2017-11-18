05492644c661d86ca3005d46615b62b8d9f5de69
kelvin8wong <kelvin8wong@gmail.com>
2017-11-18 22:14:56 +0000

    map-column to be fixed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Map from './Map.js';
 
@@ -8,10 +7,6 @@ class App extends Component {
      
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
         <div className="map-column">
           <Map 
             isMarkerShown
