130c8d1335f4531f41872bc2e6a25a1729469efd
kelvin8wong <kelvin8wong@gmail.com>
2017-11-18 03:23:03 +0000

    able to get the map loading

--- src/App.js
+++ src/App.js
@@ -5,17 +5,13 @@ import Map from './Map.js';
 
 class App extends Component {
   render() {
-    const style = {
-      width: '100vw',
-      height: '100vh'
-    }
     return (
-      <div style={style} className="App">
+      <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <Map google={this.props.google} />
+         <Map />
       </div>
     );
   }
--- src/Map.js
+++ src/Map.js
@@ -1,25 +1,33 @@
-import React, { Component } from 'react';
-import GoogleMapReact from 'google-map-react';
- 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
- 
-class Map extends Component {
- 
- 
-  render() {
-    return (
-      <GoogleMapReact
-        defaultCenter={this.props.center}
-        defaultZoom={this.props.zoom}
-      >
-        <AnyReactComponent
-          lat={59.955413}
-          lng={30.337844}
-          text={'Kreyser Avrora'}
-        />
-      </GoogleMapReact>
-    );
-  }
-}
+import React from 'react';
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker
+} from "react-google-maps";
+import _ from "lodash";
+import { compose, withProps } from "recompose";
 
-export default Map;
+const MyMapComponent = compose(
+  withProps({
+    googleMapURL:
+      "https://maps.googleapis.com/maps/api/js?key=AIzaSyC4R6AN7SmujjPUIGKdyao2Kqitzr1kiRg&v=3.exp&libraries=geometry,drawing,places",
+    loadingElement: <div style={{ height: `100%` }} />,
+    containerElement: <div style={{ height: `400px` }} />,
+    mapElement: <div style={{ height: `100%` }} />
+  }),
+  withScriptjs,
+  withGoogleMap
+)(props => (
+  <GoogleMap defaultZoom={8} defaultCenter={{ lat: -34.397, lng: 150.644 }}>
+    <Marker position={{ lat: -34.397, lng: 150.644 }} />
+  </GoogleMap>
+));
+
+const enhance = _.identity;
+
+const ReactGoogleMaps = () => [
+  <MyMapComponent key="map" />
+];
+
+export default enhance(ReactGoogleMaps);
