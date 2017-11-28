462a720615b43e588c70d13e7d92f9fe980687b4
broodmeister <isaiahchrst80@gmail.com>
2017-11-28 18:50:36 +0000

    Resized the elements on the index page.

--- src/RestaurantList.js
+++ src/RestaurantList.js
@@ -16,7 +16,7 @@ class RestaurantList extends Component {
   return (
     <div>
       <span className="button is-primary is-inverted is-outlined" style={{display:this.props.showFavourites ? 'block' : 'none'}}><h2> Favourites Vegetarian Restaurants in Town</h2></span>
-      <span className="button is-primary is-inverted is-outlined" style={{display:this.props.showFavourites ? 'none' : 'block'}}><h2> All Top Vegetarian Restaurants in Town</h2></span>
+      <span className="button is-primary is-inverted is-outlined" style={{display:this.props.showFavourites ? 'none' : 'block'}}><h2> Suggestions for nearby vegetarian restaurants</h2></span>
 
       <hr></hr>
       <ol className="restaurant-list">
