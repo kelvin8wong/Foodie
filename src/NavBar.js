a2e2b636a5b9a6907c37cf6cc66ac52d34e5322c
broodmeister <isaiahchrst80@gmail.com>
2017-11-29 02:59:46 +0000

    Fixed persisting bug and overhauled index.

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,16 +1,15 @@
 import React, { Component } from 'react';
 import RestaurantLogin from './RestaurantLogin';
 import RestaurantSignup from './RestaurantSignup';
-import { Modal, Button, Alert } from 'react-bootstrap';
+import { Modal, Button } from 'react-bootstrap';
 class NavBar extends Component {
   constructor(props) {
     super(props)
     this.state = {
       member: "",
       loginOpen: false,
-      signupOpen: false,
-      alertLogin: false,
-      alertSignup: false
+      signupOpen: false
+
     }
   }
 
@@ -33,13 +32,12 @@ class NavBar extends Component {
     .then((res) => res.json())
     .then((res) => {
       if (res == "1") {
-        self.setState({member: loginParams.member});
-        self.props.onMemberLogin(this.state.member);
-        console.log('Logged in successful:',res , loginParams.member);
-        self.setState({loginOpen : false})
+           self.setState({member: loginParams.member});
+           self.props.onMemberLogin(this.state.member);
+           console.log('Logged in successful:',res , loginParams.member);
+           self.setState({loginOpen : false})
       } else {
-        console.log('Failed logging in', res);
-        this.setState({alertLogin: true})
+          console.log('Failed logging in', res);
       }
     })
     .catch((err) => {
@@ -59,7 +57,6 @@ class NavBar extends Component {
     .then(res => res.json())
     .then((res) => {
       if (res == "1") {
-        this.props.onMemberLogin();
         this.setState({member: ""});
       }
       console.log("logout status: ", res);
@@ -83,9 +80,7 @@ class NavBar extends Component {
       if (res == "1") {
         this.login({member: signupParams.member, password: signupParams.password });
       } else {
-        this.setState({
-          alertSignup: true
-        })
+        console.log("This username has already been registered. Please choose another.");
       }
     })
   }
@@ -102,39 +97,22 @@ class NavBar extends Component {
   closeSignup = () =>{
     this.setState({signupOpen: false})
   }
-  closeAlertLogin() {
-    this.setState({ alertLogin: false });
-  }
-  closeAlertSignup() {
-    this.setState({ alertSignup: false });
-  }
+
 
   render() {
-    const alertLoginMessage = this.state.alertLogin ? 
-      <Alert bsStyle="danger" onDismiss={this.closeAlertLogin.bind(this)}> 
-        <h4>Invalid Username / Password</h4>
-      </Alert> : <div></div>
-
-    const alertSignupMessage = this.state.alertSignup ? 
-    <Alert bsStyle="danger" onDismiss={this.closeAlertSignup.bind(this)}> 
-      <h4> Username has been taken</h4>
-    </Alert> : <div></div>
-
     if (this.state.member == false) {
       return (
       <header>
         <nav className="navbar fixed-top navbar-expand-lg navbar-dark">
-          <a href="/"><img className="foodie-logo" src="https://image.ibb.co/gkO0q6/3db4d841_a8bf_454f_a12b_73ff1527ebac.png" alt="logo"/></a>
+          <a className="foodie-logo" href="/"><img src="https://image.ibb.co/hRj6DR/rsz_7492029d_b54e_4274_9fbd_694aa19dd161.png" alt="logo"/></a>
           <div className="login-signup">
             <Button onClick={this.openLogin}>Login</Button>
             <Button bsStyle="warning" onClick={this.openSignup}>Signup</Button>
             <Modal show={this.state.loginOpen}>
-                {alertLoginMessage}
                 <RestaurantLogin onLogin={this.login} />
                 <Button onClick={this.closeLogin}>Close</Button>
             </Modal>
             <Modal show={this.state.signupOpen} onHide={this.closeSignup}>
-              {alertSignupMessage}
               <RestaurantSignup onSignup={this.signup} />
               <Button onClick={this.closeSignup}>Close</Button>
             </Modal>
@@ -146,7 +124,7 @@ class NavBar extends Component {
       return(
         <header>
           <nav className="navbar fixed-top navbar-expand-lg navbar-dark">
-            <a href="/"><img className="foodie-logo" src="https://image.ibb.co/gkO0q6/3db4d841_a8bf_454f_a12b_73ff1527ebac.png" alt="logo"/></a>
+            <a href="/"><img src="https://image.ibb.co/hRj6DR/rsz_7492029d_b54e_4274_9fbd_694aa19dd161.png" alt="logo"/></a>
             <div className="navbar-brand">Welcome, {this.state.member}</div>
             <Button bsStyle="primary" onClick={this.logout.bind(this)}> Logout</Button>
           </nav>
