edab485830cad9808d00bed85c02a2010d63758a
kelvin8wong <kelvin8wong@gmail.com>
2017-11-29 04:05:56 +0000

    fixed styling on navbar

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
 import RestaurantLogin from './RestaurantLogin';
 import RestaurantSignup from './RestaurantSignup';
-import { Modal, Button } from 'react-bootstrap';
+import { Modal, Button, Alert } from 'react-bootstrap';
 class NavBar extends Component {
   constructor(props) {
     super(props)
     this.state = {
       member: "",
       loginOpen: false,
-      signupOpen: false
-
+      signupOpen: false,
+      alertLogin: false,
+      alertSignup: false
     }
   }
 
@@ -32,12 +33,13 @@ class NavBar extends Component {
     .then((res) => res.json())
     .then((res) => {
       if (res == "1") {
-           self.setState({member: loginParams.member});
-           self.props.onMemberLogin(this.state.member);
-           console.log('Logged in successful:',res , loginParams.member);
-           self.setState({loginOpen : false})
+        self.setState({member: loginParams.member});
+        self.props.onMemberLogin(this.state.member);
+        console.log('Logged in successful:',res , loginParams.member);
+        self.setState({loginOpen : false})
       } else {
-          console.log('Failed logging in', res);
+        console.log('Failed logging in', res);
+        this.setState({alertLogin: true})
       }
     })
     .catch((err) => {
@@ -57,6 +59,7 @@ class NavBar extends Component {
     .then(res => res.json())
     .then((res) => {
       if (res == "1") {
+        this.props.onMemberLogin();
         this.setState({member: ""});
       }
       console.log("logout status: ", res);
@@ -80,7 +83,9 @@ class NavBar extends Component {
       if (res == "1") {
         this.login({member: signupParams.member, password: signupParams.password });
       } else {
-        console.log("This username has already been registered. Please choose another.");
+        this.setState({
+          alertSignup: true
+        })
       }
     })
   }
@@ -97,9 +102,24 @@ class NavBar extends Component {
   closeSignup = () =>{
     this.setState({signupOpen: false})
   }
-
-
+  closeAlertLogin() {
+    this.setState({ alertLogin: false });
+  }
+  closeAlertSignup() {
+    this.setState({ alertSignup: false });
+  }
+  
   render() {
+    const alertLoginMessage = this.state.alertLogin ? 
+      <Alert bsStyle="danger" onDismiss={this.closeAlertLogin.bind(this)}> 
+        <h4>Invalid Username / Password</h4>
+      </Alert> : <div></div>
+
+    const alertSignupMessage = this.state.alertSignup ? 
+    <Alert bsStyle="danger" onDismiss={this.closeAlertSignup.bind(this)}> 
+      <h4> Username has been taken</h4>
+    </Alert> : <div></div>
+
     if (this.state.member == false) {
       return (
       <header>
@@ -109,10 +129,12 @@ class NavBar extends Component {
             <Button onClick={this.openLogin}>Login</Button>
             <Button bsStyle="warning" onClick={this.openSignup}>Signup</Button>
             <Modal show={this.state.loginOpen}>
+                {alertLoginMessage}
                 <RestaurantLogin onLogin={this.login} />
                 <Button onClick={this.closeLogin}>Close</Button>
             </Modal>
             <Modal show={this.state.signupOpen} onHide={this.closeSignup}>
+              {alertSignupMessage}
               <RestaurantSignup onSignup={this.signup} />
               <Button onClick={this.closeSignup}>Close</Button>
             </Modal>
@@ -137,4 +159,3 @@ class NavBar extends Component {
 }
 
 export default NavBar
-
