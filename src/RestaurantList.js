c640010ba64707068b0d59637b274cf44b9b270b
broodmeister <isaiahchrst80@gmail.com>
2017-11-29 19:28:30 +0000

    Changed header text.

--- src/RestaurantList.js
+++ src/RestaurantList.js
@@ -15,8 +15,8 @@ class RestaurantList extends Component {
 		})
 
   const message = this.props.showFavourites ?
-  <span><h2 className="favourite-venues"> My Favourite Vegetarian Restaurants in Town</h2></span> :
-  <span><h2 className="venue-suggestions"> Suggestions for nearby vegetarian restaurants</h2></span>
+  <span><h2 className="favourite-venues">My Favourite Vegetarian Restaurants in Town</h2></span> :
+  <span><h2 className="venue-suggestions">Vegetarian Restaurants In Your Area</h2></span>
 
   return (
     <div>
